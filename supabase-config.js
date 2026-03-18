96475dbecf2f9d2e312d6c5711971216b5bf8386
Mateus Flausino <flauzinomateus20@gmail.com>
2026-03-18 00:25:13 -0300

    atualizacao da v.3.2.1  app

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.UFTM_SUPABASE_CONFIG = {
   url: "https://gecyqtkzbhcfxqupecvz.supabase.co",
-  anonKey: "COLE_AQUI_A_PUBLISHABLE_KEY_COMPLETA",
+  anonKey: "sb_publishable_WxzyNSO3j8EpL2J0iasVKQ_CN_NVrGk",
   bucket: "student-pdfs",
 };
